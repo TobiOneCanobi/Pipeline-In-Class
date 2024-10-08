1eac6f35bc342b2e50f344b09317828226a5a022
TobiOneCanobi <vlogkongen@gmail.com>
2024-10-08 15:46:55 +0200

    changed port form 7000 to 7070

--- src/main/java/dk/lyngby/util/ApiProps.java
+++ src/main/java/dk/lyngby/util/ApiProps.java
@@ -10,7 +10,7 @@ public class ApiProps {
 
 
     // == API CONFIG ==
-    public static final int PORT = 7000;
+    public static final int PORT = 7070;
     public static final String API_CONTEXT = "/api/v1";
 
 }
